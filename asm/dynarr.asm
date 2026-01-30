691ea50a57ca041d1b07c22957dd9f587bca3dea
hi-anki <managemee@protonmail.com>
2026-01-30 17:28:53 +0530

    done with pushOne

--- asm/dynarr.asm
+++ asm/dynarr.asm
@@ -140,3 +140,91 @@ extend:
 .ret_block:
   leave
   ret
+
+
+.section .text
+.global pushOne
+.type pushOne, @function
+
+# Function Parameters:
+#   rdi=&arr   (ptr to the dynamic array struct)
+#   rsi=&value (ptr to the value to be pushed in the arr->ptr, type:void)
+pushOne:
+  push rbp
+  mov  rbp, rsp
+  push r14
+  push r15
+
+  test rdi, rdi      # !arr
+  jz .init_first
+
+  mov  rcx, [rdi + 8*1]     # rcx=arr->elem_size
+  test rcx, rcx             # !arr->elem_size
+  jz   .init_first
+
+  mov  r8, [rdi + 8*3]      # r8=arr->capacity
+  test r8, r8               # !arr->capacity
+  jz   .init_first
+
+# if (arr->count+1 > arr->capacity)
+#   We need two registers here, one for (arr->count+1) and the other for arr->capacity
+#   In this region of code, only arr->count and arr->capacity are active, that means, we can overwrite rcx which holds arr->elem_size currently (as the original value is intact at rdi + 8*1)
+  mov rcx, [rdi + 8*2]      # rcx=arr->count
+  add rcx, 1                # rcx=arr->count+1
+  cmp rcx, r8
+
+# This is an important decision point.
+#   If I use `jg .call_extend`, I'll have to create a separate label for memcpy stuff because calling extend doesn't mean we will skip the rest of the code. If extend was successful, we have to go to the memcpy part, always.
+#   If I make `.call_extend` the happy path, I can remove it as a label and only keep the memcpy label, to which we will jump if it were less than or eequal to. This way, we can prevent extra branching.
+  jle  .memcpy_label        # if (arr->count+1 <= arr->capacity)
+
+# extend takes 2 argument (ptr to the dynarr struct, add_bytes)
+#   Since we are only pushing one element add_bytes (rsi) would be 1
+#   Notice the argument is exactly what pushOne has received in rdi, so rdi needs no manipulation, but that model of thikning is wrong. As we call extend, pushOne becomes a caller-saved register and as caller it must preserve it if it want to use it later, so we'll preserve rdi in a callee-saved register (r14)
+#   Since rsi contains the ptr to the value to push, we need to save rsi in a calle-saved register (r15)
+#   In this region, only &arr (or rdi) is active. While arr->count (rcx) is still of use in later regions, arr->capacity (r8) is of no use now, so we can overwrite r8 to preserve rsi
+  mov  r14, rdi    # preserve rdi (&arr)
+  mov  r15, rsi    # preserve rsi (&value_to_push)
+  mov  rsi, 1
+  call extend     # extend(arr, 1)
+
+  test rax, rax
+  jnz  .ret_block     # No need to set rax as it is already set with appropriate return value
+
+# `void *dest = (char*)arr->ptr + (arr->count * arr->elem_size);`
+#   This line is about computing the destination memory address where memcpy will start copying memory
+#   3 values are live here: arr->ptr, arr->count and arr->elem_size
+#   arr->count is already live in rcx but we can't rely on it as it is a caller-saved register and we've made a call to extend which could've polluted it. We'll populate rcx again from the memory directly
+#   We need to obtain arr->ptr fresh.
+#   Since arr->elem_size has been overwritten, we need it fresh again. Since we need it as 3rd arg to memcpy, we'll use rdx for it
+
+  # Set arg3 (rsi=arr->elem_size)
+  mov  rdx, [r14 + 8*1]     # arr->elem_size
+
+  # Set arg2 (rsi=&value)
+  mov  rsi, r15            # (ptr to value)
+
+  # Set arg1 (rdi=&dest)
+  mov  rcx, [r14 + 8*2]    # arr->count
+  imul rcx, rdx            # rcx = arr->count * arr->elem_size
+  mov  rdi, [r14 + 8*0]    # arr->ptr
+  add  rdi, rcx            # rdi=(arr->ptr + (arr->count * arr->elem_size))
+
+  call memcpy@PLT
+
+  # Update arr->count (++)
+  mov rcx, QWORD PTR [r14 + 8*2]
+  add rcx, 1
+  mov QWORD PTR [r14 + 8*2], rcx
+
+  xor rax, rax      # SUCCESS
+  jmp .ret_block
+
+.init_first:
+  mov rax, -5
+
+.ret_block:
+  pop r15
+  pop r14
+  leave
+  ret
