67412957a85cb5170a69d750d1245796e9ea38b8
hi-anki <managemee@protonmail.com>
2026-01-31 22:55:32 +0530

    changes

--- dynarr/dynarr.c
+++ dynarr/dynarr.c
@@ -1,13 +1,12 @@
 #include "dynarr.h"
 
-DynArrStatus init(DynArr *arr, size_t elem_size, size_t cap){
-  if (arr->capacity != 0) return ALREADY_INIT;
+DynArrStatus init(DynArr* arr, size_t elem_size, size_t cap){
+  if (!arr->capacity) return -ALREADY_INIT;
+  if (!elem_size || !cap) return -INVALID_SIZES;
+  if (cap > SIZE_MAX/elem_size) return -SIZEMAX_OVERFLOW;
 
-  if (elem_size == 0 || cap == 0) return INVALID_SIZES;
-  if (cap > SIZE_MAX/elem_size) return SIZEMAX_OVERFLOW;
-
-  void *ptr = malloc(cap * elem_size);
-  if (!ptr) return MALLOC_FAILED;
+  void* ptr = malloc(cap * elem_size);
+  if (!ptr) return -MALLOC_FAILED;
 
   arr->ptr = ptr;
   arr->elem_size = elem_size;
@@ -17,16 +16,16 @@ DynArrStatus init(DynArr *arr, size_t elem_size, size_t cap){
   return SUCCESS;
 }
 
-DynArrStatus extend(DynArr *arr, size_t add_bytes){
-  if (!arr || arr->capacity == 0) return INIT_FIRST;
+DynArrStatus extend(DynArr* arr, size_t add_bytes){
+  if (!arr || !arr->ptr) return -INIT_FIRST;
   if (arr->count+add_bytes <= arr->capacity) return SUCCESS;        // Works with add_bytes=0 as well
 
   size_t total = arr->count+add_bytes;
-  size_t cap = arr->capacity;
+  size_t cap   = arr->capacity;
   while (cap < total) cap *= 2;
 
-  void *tmp = realloc(arr->ptr, cap*arr->elem_size);
-  if (!tmp) return REALLOC_FAILED;
+  void* tmp = realloc(arr->ptr, cap*arr->elem_size);
+  if (!tmp) return -REALLOC_FAILED;
 
   arr->ptr = tmp;
   arr->capacity = cap;
@@ -34,123 +33,115 @@ DynArrStatus extend(DynArr *arr, size_t add_bytes){
   return SUCCESS;
 }
 
-DynArrStatus pushOne(DynArr *arr, const void *value){
-  if (!arr || !arr->elem_size || !arr->capacity) return INIT_FIRST;
+DynArrStatus pushOne(DynArr* arr, const void* value){
+  if (!arr || !arr->ptr) return -INIT_FIRST;
+  if (!value) return -INVALID_PUSHREQUEST;
 
   if (arr->count+1 > arr->capacity){
     int res = extend(arr, 1);
     if (res != SUCCESS) return res;
   }
 
-  void *dest = (char*)arr->ptr + (arr->count * arr->elem_size);
+  void* dest = (char*)arr->ptr + (arr->count * arr->elem_size);
   memcpy(dest, value, arr->elem_size);
-  arr->count++;
 
+  arr->count++;
   return SUCCESS;
 }
 
-DynArrStatus pushMany(DynArr *arr, const void *elements, size_t count){
-  if (!arr || !arr->elem_size) return INIT_FIRST;
-  if (count == 0) return INVALID_COUNT;
+DynArrStatus pushMany(DynArr* arr, const void* elements, size_t count){
+  if (!arr || !arr->ptr)   return -INIT_FIRST;
+  if (!elements || !count) return -INVALID_PUSHREQUEST;
 
   int res = extend(arr, count);
   if (res != SUCCESS) return res;
 
-  void *dest = (char*)arr->ptr + (arr->count*arr->elem_size);
-  memcpy(dest, elements, count*arr->elem_size);
-  arr->count += count;
+  void* dest = (char*)arr->ptr + (arr->count*arr->elem_size);
+  memcpy(dest, elements, count * arr->elem_size);
 
+  arr->count += count;
   return SUCCESS;
 }
 
-const void *getelement(const DynArr *arr, size_t idx){
+int boundcheck(size_t lb, size_t ub, size_t idx){ return (idx >= lb && idx < ub); }
+
+const void* getelement(const DynArr* arr, size_t idx){
   if (!arr || !arr->ptr) return NULL;
-  if (boundcheck(0, arr->count, idx)) return NULL;
+  if (boundcheck(0UL, arr->count, idx)) return NULL;
 
   return ((char*)arr->ptr + (idx * arr->elem_size));
 }
 
-size_t getarrlen(const DynArr *arr){ return arr->count; }
+int isempty(const DynArr* arr){ return (arr->count == 0) ? 0 : 1; }
 
-size_t getcap(const DynArr *arr){ return arr->capacity; }
+DynArrStatus setidx(DynArr* arr, const void* value, size_t idx){
+  if (!arr || !arr->ptr) return -INIT_FIRST;
+  if (!value) return -INVALID_PUSHREQUEST;
+  if (boundcheck(0UL, arr->count, idx) != 1) return -INVALID_IDX;
 
-DynArrStatus isempty(const DynArr *arr){ return (arr->count == 0) ? IS_EMPTY : ISNOT_EMPTY; }
-
-int boundcheck(size_t lb, size_t ub, size_t idx){ return (idx >= lb && idx < ub); }
-
-DynArrStatus setidx(DynArr *arr, const void *value, size_t idx){
-  if (!arr || arr->count == 0 || arr->capacity == 0) return INIT_FIRST;
-  if (boundcheck(0, arr->count, idx) != 1) return INVALID_IDX;
-
-  void *dest = (char*)arr->ptr + (idx*arr->elem_size);
+  void  *dest = (char*)arr->ptr + (idx*arr->elem_size);
   memcpy(dest, value, arr->elem_size);
   return SUCCESS;
 }
 
-DynArrStatus bytecopy(const DynArr *main, DynArr *copy){
-  if (!main || !copy || !main->ptr) return EMPTY_PTR;
-  if (extend(copy, main->count) != SUCCESS) return REALLOC_FAILED;
-  if (main->elem_size != copy->elem_size) return TYPES_DONT_MATCH;
+DynArrStatus mergedyn2dyn(const DynArr* src, DynArr* dest){
+  if (!src || !src->ptr || !dest || !dest->ptr) return -INIT_FIRST;
+  if (src->elem_size != dest->elem_size) return -TYPES_DONT_MATCH;
 
-  memcpy((char*)copy->ptr, main->ptr, main->count*main->elem_size);
-  copy->count = main->count;
+  int res = extend(dest, src->count);
+  if (res != SUCCESS) return res;
+
+  void* dptr = (char*)dest->ptr + dest->count*dest->elem_size;
+  size_t bytes = src->count * src->elem_size;
+  memcpy(dptr, src->ptr, bytes);
+
+  dest->count += src->count;
   return SUCCESS;
 }
 
-DynArrStatus merge(DynArr *arr1, const DynArr *arr2){
-  if (!arr1 || !arr1->ptr || !arr2 || !arr2->ptr) return INIT_FIRST;
-  if (extend(arr1, arr2->count) != SUCCESS) return REALLOC_FAILED;
+DynArrStatus export2stack(const DynArr* dynarr, void** stackarr){
+  if (!dynarr || !dynarr->ptr) return -INIT_FIRST;
 
-  void *dest = (char*)arr1->ptr + arr1->count*arr1->elem_size;
-  memcpy(dest, arr2->ptr, arr2->count*arr2->elem_size);
-  arr1->count += arr2->count;
-
+  memcpy(*stackarr, dynarr->ptr, dynarr->count * dynarr->elem_size);
   return SUCCESS;
 }
 
-DynArrStatus export2stack(const DynArr *dynarr, void **stackarr){
-  if (!dynarr || !dynarr->ptr) return INIT_FIRST;
-
-  memcpy(*stackarr, dynarr->ptr, dynarr->count*dynarr->elem_size);
-  return SUCCESS;
-}
-
-DynArrStatus insertidx(DynArr *arr, const void *value, size_t idx){
-  if (!arr || !arr->ptr) return INIT_FIRST;
-  if (arr->count == SIZE_MAX) return SIZEMAX_OVERFLOW;
-  if (boundcheck(0, arr->count, idx) != 1) return INVALID_IDX;
+DynArrStatus insertidx(DynArr* arr, const void* value, size_t idx){
+  if (!arr || !arr->ptr) return -INIT_FIRST;
+  if (!value) return -INVALID_PUSHREQUEST;
+  if (boundcheck(0UL, arr->count, idx) != 1) return -INVALID_IDX;
 
   if (arr->count == arr->capacity){
     int res = extend(arr, 1);
     if (res != SUCCESS) return res;
   }
 
-  void *dest = (char*)arr->ptr + (idx + 1)*arr->elem_size;
-  void *src  = (char*)arr->ptr + idx*arr->elem_size;
-  size_t bytes = (arr->count - idx)*arr->elem_size;
+  void* dest = (char*)arr->ptr + (idx + 1)*arr->elem_size;
+  void* src  = (char*)arr->ptr + idx*arr->elem_size;
+  size_t bytes = (arr->count - idx) * arr->elem_size;
   memmove(dest, src, bytes);
 
   setidx(arr, value, idx);
   arr->count++;
+
   return SUCCESS;
 }
 
-DynArrStatus removeidx(DynArr *arr, size_t idx){
-  if (!arr || !arr->ptr || arr->count == 0) return INIT_FIRST;
-  if (boundcheck(0, arr->count, idx) != 1) return INVALID_IDX;
+DynArrStatus removeidx(DynArr* arr, size_t idx){
+  if (!arr || !arr->ptr) return -INIT_FIRST;
+  if (boundcheck(0UL, arr->count, idx) != 1) return -INVALID_IDX;
 
-  void *dest = (char*)arr->ptr + idx*arr->elem_size;
-  void *src = (char*)arr->ptr + (idx+1)*arr->elem_size;
+  void* dest = (char*)arr->ptr + idx*arr->elem_size;
+  void* src  = (char*)arr->ptr + (idx+1)*arr->elem_size;
   size_t bytes = (arr->count-idx-1)*arr->elem_size;
   memmove(dest, src, bytes);
-  arr->count--;
 
+  arr->count--;
   return SUCCESS;
 }
 
-// Array can be reused
-DynArrStatus clearArr(DynArr *arr){
-  if (!arr || !arr->ptr) return EMPTY_PTR;
+DynArrStatus clearArr(DynArr* arr){
+  if (!arr || !arr->ptr) return -INIT_FIRST;
 
   arr->ptr = NULL;
   arr->count = 0;
@@ -159,9 +150,8 @@ DynArrStatus clearArr(DynArr *arr){
   return SUCCESS;
 }
 
-// Deallocates the array
-DynArrStatus freeArr(DynArr *arr){
-  if (!arr->ptr) return EMPTY_PTR;
+DynArrStatus freeArr(DynArr* arr){
+  if (!arr || !arr->capacity) return -INIT_FIRST;
 
   free(arr->ptr);
   arr->ptr = NULL;
--- dynarr/dynarr.h
+++ dynarr/dynarr.h
@@ -9,7 +9,7 @@
 
 // Container for dynamic arrays
 typedef struct {
-  void *ptr;
+  void*  ptr;
   size_t elem_size;
   size_t count;
   size_t capacity;
@@ -17,73 +17,62 @@ typedef struct {
 
 // Return status codes
 typedef enum {
-  SUCCESS=0,
-  MALLOC_FAILED=-1,
-  EMPTY_PTR=-2,
-  INIT_FIRST=-3,
-  REALLOC_FAILED=-4,
-  INVALID_IDX=-5,
-  SIZEMAX_OVERFLOW=-6,
-  ALREADY_INIT=-7,
-  IS_EMPTY=-8,
-  ISNOT_EMPTY=-9,
-  TYPES_DONT_MATCH=-10,
-  INVALID_SIZES=-11,
-  INVALID_COUNT=-12
+  SUCCESS,
+  ALREADY_INIT,
+  INVALID_SIZES,
+  SIZEMAX_OVERFLOW,
+  MALLOC_FAILED,
+  INIT_FIRST,
+  REALLOC_FAILED,
+  INVALID_PUSHREQUEST,
+  INVALID_IDX,
+  TYPES_DONT_MATCH,
 } DynArrStatus;
 
 // Initialize the dynamic array
-DynArrStatus init(DynArr *arr, size_t elem_size, size_t cap);
+DynArrStatus init(DynArr* arr, size_t elem_size, size_t cap);
 
 // Extend the capacity of an existing dynamic array
-DynArrStatus extend(DynArr *arr, size_t required);
+DynArrStatus extend(DynArr* arr, size_t required);
 
 // Push one element to the end of the dynamic array (append)
-DynArrStatus pushOne(DynArr *arr, const void *value);
+// Caller must ensure type-correctness of the value to be pushed
+DynArrStatus pushOne(DynArr* arr, const void* value);
 
 // Push an array of elements to the end of the dynamic array (bulk-append)
-DynArrStatus pushMany(DynArr *arr, const void *elements, size_t count);
-
-// Merge a VLA (stack-allocated) in a dynamic array
-#define mergeVLA pushMany;
-
-// Getter function to access the dynamic array ptr
-const void *getelement(const DynArr *arr, size_t idx);
-
-// Getter function to access the dynamic array element count
-size_t getarrlen(const DynArr *arr);
-
-// Getter function to access the dynamic array capacity
-size_t getcap(const DynArr *arr);
-
-// Checks for empty dynamic arrays; -1 for empty && 0 for allocated
-DynArrStatus isempty(const DynArr *arr);
+// Caller must ensure type-correctness of the values to be pushed
+DynArrStatus pushMany(DynArr* arr, const void* elements, size_t count);
 
 // Function to check bounds
 int boundcheck(size_t lb, size_t ub, size_t idx);
 
+// Getter function to access the dynamic array ptr
+const void* getelement(const DynArr* arr, size_t idx);
+
+// Checks for empty dynamic arrays; -1 for empty && 0 for allocated
+int isempty(const DynArr* arr);
+
 // Function to set the element by indexing (same as array-subscripting)
-DynArrStatus setidx(DynArr *arr, const void *value, size_t idx);
-
-// Perform deep copy of dynamic array into another dynamic array
-DynArrStatus bytecopy(const DynArr *main, DynArr *copy);
-
-// Merge two dynamic arrays (arr2 is extended into arr1)
-DynArrStatus merge(DynArr *arr1, const DynArr *arr2);
-
-// Export a dynamic array to a stack-allocated array
-DynArrStatus export2stack(const DynArr *dynarr, void **stackarr);
+// Caller must ensure type-correctness of the value
+DynArrStatus setidx(DynArr* arr, const void* value, size_t idx);
 
 // Insert element at an arbitrary index in the array
-DynArrStatus insertidx(DynArr *arr, const void *value, size_t idx);
+// Caller must ensure type-correctness of the value to be pushed
+DynArrStatus insertidx(DynArr* arr, const void* value, size_t idx);
 
 // Remove element at an arbitrary index in the array
-DynArrStatus removeidx(DynArr *arr, size_t idx);
+DynArrStatus removeidx(DynArr* arr, size_t idx);
+
+// Merge two dynamic arrays (arr2 is extended into arr1)
+DynArrStatus mergedyn2dyn(const DynArr* src, DynArr* dest);
+
+// Export a dynamic array to a stack-allocated array
+DynArrStatus export2stack(const DynArr* dynarr, void** stackarr);
 
 // Makes the array reusable
-DynArrStatus clearArr(DynArr *arr);
+DynArrStatus clearArr(DynArr* arr);
 
 // Deallocates the array
-DynArrStatus freeArr(DynArr *arr);
+DynArrStatus freeArr(DynArr* arr);
 
 #endif
